27da6eb35b466e69e296ca6d476eb452b897ea86
Paulo.R.F.Pizza <117474750+paulo626@users.noreply.github.com>
2024-11-06 13:31:21 -0300

    tredretrerdyty

--- js/Banner.js
+++ js/Banner.js
@@ -3,9 +3,10 @@ const banner = document.getElementById('banner');
 // Função que verifica a rolagem
 function verificarScroll() {
     // Verifica se a rolagem foi maior que 50px
+    console.log('conecto')
     if (window.scrollY > 100) {
         banner.classList.add('fixo'); // Adiciona a classe 'fixo' se rolou mais que 50px
-        
+        console.log('foi')
     } else {
         banner.classList.remove('fixo'); // Remove a classe 'fixo' se o usuário voltou para o topo
 
@@ -15,4 +16,5 @@ function verificarScroll() {
 
 
 // Chama a função ao rolar a página
-window.onscroll = verificarScroll;
+
+window.addEventListener('scroll', verificarScroll);
--- js/Links.js
+++ js/Links.js
@@ -15,7 +15,14 @@ function verificarScrollLinks(){
         erer.style.color = 'black'
     }
 
+    if (window.scrollY > 100) {
+        links.classList.add('fixolink'); // Adiciona a classe 'fixo' se rolou mais que 50px
+    } else {
+        links.classList.remove('fixolink'); // Remove a classe 'fixo' se o usuário voltou para o topo
+
+    }
 }
 
 // Chama a função ao rolar a página
 window.onscroll = verificarScrollLinks;
+
